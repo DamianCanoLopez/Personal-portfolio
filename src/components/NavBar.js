117c109ce2dbe2f9583223dd8fbccb4e797f86a3
Damián Cano López <damiancano1811@hotmail.com>
2023-04-27 17:06:03 -0500

    ready visualizations and functions, and creation of new components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,11 +46,11 @@ export const NavBar = () => {
               <Nav className="ms-auto">
                 <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                <Nav.Link href="#project" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
               </Nav>
               <span className="navbar-text">
                 <div className="social-icon">
-                  <a href="#"><img src={navIcon1} alt="" /></a>
+                  <a href="https://www.linkedin.com/in/carlos-damian-cano-lopez-399812260/" target="_blank" style={{textDecoration: 'none', color: 'white'}}><img src={navIcon1} alt="" /></a>
                   <a href="#"><img src={navIcon2} alt="" /></a>
                   <a href="#"><img src={navIcon3} alt="" /></a>
                 </div>
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,17 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl, UrlWeb }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+    <a href={UrlWeb} target="_blank" style={{textDecoration: 'none', color: 'white'}}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </a>
+    </Col>
+  )
+}
